44179022377bc7c66bbdc1881122525fca03e6fe
ade bakari <redariez@gmail.com>
2019-03-05 17:16:53 -0800

    added some html and JS

--- js/app.js
+++ js/app.js
@@ -1,34 +1,58 @@
 $(document).ready(function() {
-      var el = document.getElementById('text');
+  var el = document.getElementById('text');
+
+  $(document).on('mousedown', function(event) {
+    event.preventDefault();
+
+    switch (event.which) {
+      case 1:
+        console.log("Clicked left mouse button");
+        break;
+      case 2:
+      console.log("Clicked middle button");
+      break;
+      case 3:
+      console.log("Clicked right button");
+      break;
+    }
+
+  });
+
+  $('[data-trigger="dropdown"]').on('mouseenter', function() {
+    var submenu = $(this).parent().find('.submenu')
+    submenu.fadeIn(300);
 
 
-      $('[data-trigger="dropdown"]').on('mouseenter', function() {
-          var submenu = $(this).parent().find('.submenu')
-          submenu.fadeIn(300);
+    $('.profile-menu').on('mouseleave', function() {
+      submenu.fadeOut(300);
+    });
+  });
 
 
-          $('.profile-menu').on('mouseleave', function() {
-            submenu.fadeOut(300);
-          });
-      });
+  $('#prepend, #append, #replace').on('click', function(e) {
+    e.preventDefault();
 
-$('#prepend, #append, #replace').on('click', function(e) {
     var el = $(e.currentTarget);
     var action = el.attr('id');
-    var content = $('#text').val();
+    var content = $('.text').val();
 
 
-    if(action == "prepend") {
+    if (action == "prepend") {
       console.log("Prepending...", content)
       $('#main').prepend(content);
 
-    }else if(action == "append") {
+    } else if (action == "append") {
       console.log("Appending...")
+      $('#main').append(content);
 
-    }else if(action == "replace") {
+    } else if (action == "replace") {
       console.log("Replacing...")
+      $('#main').html(content);
+
+
     }
 
-});
+    $('.text').val('');
+  });
 
 });
