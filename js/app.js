24a29965c0b232fce2702988bcc1af4a0f48d1e6
ade bakari <redariez@gmail.com>
2019-03-19 16:22:34 -0700

    done with jquery

--- js/app.js
+++ js/app.js
@@ -1,71 +1,33 @@
 $(document).ready(function() {
-  var el = document.getElementById('text');
-  });
 
-gQuery(".my-selector").addClass("MYNEWCLASS");
-
-gQuery("#main").addClass("HELLO");
-  $(document).on('mousedown', function(event) {
-    event.stopPropagation();
-
-    if (event.which == 3) {
-
-        $('.hidden').removeClass('shown');
-
-        if($(event.target).is('img')) {
-            $('.saveimg, .newtab').addClass('shown');
-        }else if($(event.target).is('a')) {
-          $('.newtab').addClass('shown');
+      function printThis(el) {
+        if($(this).text()) {
+          console.log($(this).text());
+        }else{
+          console.log($(this).val());
         }
+      }
 
-      console.log(event.pageY, event.pageX);
 
-      $('#context').css({
-        top: event.pageY,
-        left: event.pageX
+        var el = document.getElementById('text');
+
+
+        $('[data-trigger="dropdown"]').on('mouseenter', function() {
+          var submenu = $(this).parent().find('.submenu')
+          submenu.fadeIn(300);
+
+
+          $('.profile-menu').on('mouseleave', function() {
+            submenu.fadeOut(300);
+          });
+        });
+
+        $('input').focusin(printThis);
+
+        $('input').css({
+          background: '#f00',
+          padding: '10px',
+          borderColor: '#000',
+        });
+
       });
-
-          $('#context').fadeIn();
-      return false;
-
-    }
-      $('#context').fadeOut();
-
-  });
-
-  $('[data-trigger="dropdown"]').on('mouseenter', function() {
-    var submenu = $(this).parent().find('.submenu')
-    submenu.fadeIn(300);
-
-
-    $('.profile-menu').on('mouseleave', function() {
-      submenu.fadeOut(300);
-    });
-  });
-
-
-  $('#prepend, #append, #replace').on('click', function(e) {
-    e.preventDefault();
-
-    var el = $(e.currentTarget);
-    var action = el.attr('id');
-    var content = $('.text').val();
-
-
-    if (action == "prepend") {
-      console.log("Prepending...", content)
-      $('#main').prepend(content);
-
-    } else if (action == "append") {
-      console.log("Appending...")
-      $('#main').append(content);
-
-    } else if (action == "replace") {
-      console.log("Replacing...")
-      $('#main').html(content);
-
-
-    }
-
-    $('.text').val('');
-  });
